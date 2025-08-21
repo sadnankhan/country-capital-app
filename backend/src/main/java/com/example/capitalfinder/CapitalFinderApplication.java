61c722eb50d17e8602424bd80cbff15e356fcdbf
sadnankhan <sadnan.kingkhan1@gmail.com>
2025-08-21 15:51:13 +1000

    Create CapitalFinderApplication.java

---
+++ backend/src/main/java/com/example/capitalfinder/CapitalFinderApplication.java
@@ -0,0 +1,11 @@
+package com.example.capitalfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalFinderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalFinderApplication.class, args);
+    }
+}
